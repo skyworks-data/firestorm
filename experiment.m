69b8e4b11bbb29ce171ba3018bec4027b03a4331
ekraihan <ekraihan@iupui.edu>
2018-03-25 19:21:56 -0400

    adding auc package and readAllData

--- experiment.m
+++ experiment.m
@@ -6,8 +6,13 @@
 % train = readtable('train_sample.csv', opts);
 % sortedTrain = sortrows(train,Feature.ClickTime.name);
 
-nb = fitcnb(train(1:6000,2:5),train(1:6000,end),'Prior','empirical','CategoricalPredictors','all');
+nb = fitcnb(data(:,2:5),data(:,end),'CategoricalPredictors','all');
 
-pred = predict(nb,train(6001:end,2:5));
-roc(train.is_attributed(6001:end)',double(pred'))
+pred = predict(nb, test(:,2:5));
+roc(data.is_attributed(end-1000000:end)',double(pred'))
 
+is_attributed = pred;
+click_id = ids;
+click_id = click_id.click_id;
+submission = table(click_id,is_attributed);
+writetable(submission, 'submission.csv');
--- util/loadPartialData.m
+++ util/loadPartialData.m
@@ -1,38 +1,28 @@
 function [train, test] = loadPartialData(fileName, numRows,cols)
     % fileName: Name of file to read in
     % numRows: Number of rows to randomly pull
-    % cols:    A 1-D cell array containing type Features
+    % cols:    A 1-D cell array containing type Feature
     % TODO: Some optimization of data types being read in may help save space
     
 %     [~, strRowNum] = system(['wc -l <', fileName]);
 %     nTrainRows = str2double(strRowNum)-1;
 %     fileID = fopen(fileName);
 % 
-%     csvFormat = cell2mat(arrayfun(@(x) x.format, enumeration('Feature'), 'UniformOutput',false)');
-% 
-%     train = [];
-%     tic
-%     for i = 1:numRows
-%         train = [train; textscan(fileID, csvFormat, 10000, ...
-%                  'Delimiter', ',', ...
-%                  'HeaderLines', 100)];
-%     end
-%     toc
+    NUM_SKIP = 4;
+    
     dataStore = datastore(fileName);
-    dataStore.VariableNames = arrayfun(@(x) x.name, enumeration('Feature'), 'UniformOutput',false);
     dataStore.TreatAsMissing = '';
     dataStore.ReadSize = 1000000;
 	dataStore.SelectedVariableNames = cellfun(@(x) x.name, cols, 'UniformOutput',false);
 	dataStore.SelectedFormats = cellfun(@(x) x.format, cols, 'UniformOutput',false);
     
+    nPar = numpartitions(dataStore);
+    nReads = floor(nPar/NUM_SKIP);
     
     train = [];
-    for i = 1:200
-%         dataStore.NumHeaderLines = randi([1 nTrainRows]);
+    for p = 1:nReads+1
         tic
-%         train = [train; readtable('train.csv', opts)];
-        train = read(dataStore);
-        size(train)
+        train = [train; read(partition(dataStore,nPar,(p-1)*NUM_SKIP+1))];
         toc
     end
     
